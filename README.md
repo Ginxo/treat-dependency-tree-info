85ae264bb50af1d87f318ddc94aef7ec42b53d91
Enrique Mingorance Cano <ginxaco@gmail.com>
2021-12-17 20:17:36 +0100

    v0.2.0 get info implementation (#3)

--- README.md
+++ README.md
@@ -1,6 +1,42 @@
 # Treat maven-dependency-plugin log information
 
-This script tries to solve the problem of consuming maven-dependency.plugin information from multiple modules.
+This script tries to solve the problem of consuming maven-dependency-plugin information from multiple modules from multiple projects.
+
+## Let's clarify the problem it tries to cover
+
+What happens whenever you want to upgrade a library but you need to analyze the side effect of this new library version with the rest of the projects in you organization? Imagine you have more than a thousand maven modules involved? How would you do it? Well... `treat-maven-dependency-plubin-log` proposes a simple and easy way to do it. Just run your `mvn dpendency:tree` command for all of your projects and save the log into a file and then run the tool to filter (by regular expressions filters) the artifacts you want to filter from maven-dependency-plugin:tree output. The maven-dependency-plugin:tree output from every module mathing the filter will be summarize in one single place module by module, like
+
+```
+###########################################
+jbpm-workitems-rest
+###########################################
+
+[INFO] org.jbpm:jbpm-workitems-rest:jar:7.64.0-SNAPSHOT
+[INFO] +- org.apache.cxf:cxf-rt-transports-http-jetty:jar:3.3.9:test
+[INFO] |  +- org.apache.cxf:cxf-core:jar:3.3.9:test
+...
+...
+
+###########################################
+kie-server-spring-boot-autoconfiguration
+###########################################
+
+[INFO] org.kie:kie-server-spring-boot-autoconfiguration:jar:7.64.0-SNAPSHOT
+[INFO] +- org.springframework.boot:spring-boot:jar:2.3.4.RELEASE:compile
+...
+
+and so on and so on...
+```
+
+### Real world example
+
+We wanted to move our [droolsjbpm-build-bootstrap](https://github.com/kiegroup/droolsjbpm-build-bootstrap) org.apache.cxf:3.3.9 dependency to org.apache.cxf:3.4.5, easy right? (See [RHPAM-4012](https://issues.redhat.com/browse/RHPAM-4012)). Then we noticed comes with `jakarta.validation:jakarta.validation-api:2.0.2` and all the projects depending on it (more than 30) are still using the old `javax.validation:validation-api:2.0.1`, but... how deep the impact for this change is? we needed to meassure it and we came to the conclusion we needed to automatize it somehow. We execute a full downstream build for this specific project just running `mvn depdendency:tree` and we stored the output in one single file but now it's time to consume this huge amount of information, so... we decided to create this tool to do the hard work for us by just running commands like
+
+`treat-maven-dependency-plubin-log -l thelogfile.log -f "\] \+- org.apache.cxf:cxf.*:jar:3.3.9" "validation-api" -p`
+`treat-maven-dependency-plubin-log -l thelogfile.log -f "\] \+- org.apache.cxf:cxf.*:jar:3.3.9" "validation-api" -e "org.kie.soup:kie-soup-dataset-api" -p`
+etc...
+
+## Instalation and execution
 
 Just install
 
@@ -18,6 +54,9 @@ being `<filePath>` and absoloute or relative path to the maven log file where th
 
 ## Arguments
 
-- `-l --log-file-path <filePath>`: The maven log file path
+- `-l, --log-file-path <filePath>`: The maven log file path
 - `-o, --output-file <filePath>`: The file path to write the output information from the execution
-- `-f --filter <regex>`: The regex to filter just the modules' dependency:tree matching with the regex. For instance `-f validator-api:\d` will filter just the modules where the dependency tree matches `validator-api:\d` regular expression.
+- `-f, --filter <regex...>`: The regex list to filter just the modules' dependency:tree matching with the regex. For instance `-f validator-api:\d` will filter just the modules where the dependency tree matches `validator-api:\d` regular expression.
+- `-e, --exclude <regex...>`: The regex list to exclude the modules' dependency:tree matching with the regex. For instance `-e validator-api:\d` will exclude all the modules where the dependency tree matches `validator-api:\d` regular expression.
+- `--skip-output`: Will skip output to be writen on any file. This invalidates the `-o` argument.
+- `-p ,--print-module-list`: It will print a summary of module lists matching the requirements from execution.
--- test/lib/dependency-tree-service.test.js
+++ test/lib/dependency-tree-service.test.js
@@ -27,21 +27,48 @@ whatever
 });
 
 describe("filterDependencyTree", () => {
-  test("simple regex", () => {
+  test("simple regex 1 matching", () => {
     // Arrange
     const dependencyTreeObject = getDependencyTreeObjectFromFile(
       path.join(__dirname, "..", "resources", "simple.log")
     );
 
     // Act
-    const result = filterDependencyTree(
-      dependencyTreeObject,
+    const result = filterDependencyTree(dependencyTreeObject, [
       "jakarta.transaction-api:jar:\\d"
-    );
+    ]);
 
     // Assert
     expect(result.length).toBe(1);
     expect(result[0].moduleId).toBe("kie-soup-commons");
+  });
+
+  test("simple regex both matching", () => {
+    // Arrange
+    const dependencyTreeObject = getDependencyTreeObjectFromFile(
+      path.join(__dirname, "..", "resources", "simple.log")
+    );
+
+    // Act
+    const result = filterDependencyTree(dependencyTreeObject, [
+      "junit:junit:jar"
+    ]);
+
+    // Assert
+    expect(result.length).toBe(2);
+  });
+
+  test("simple regex non matching", () => {
+    // Arrange
+    const dependencyTreeObject = getDependencyTreeObjectFromFile(
+      path.join(__dirname, "..", "resources", "simple.log")
+    );
+
+    // Act
+    const result = filterDependencyTree(dependencyTreeObject, ["X:X:jar"]);
+
+    // Assert
+    expect(result.length).toBe(0);
   });
 
   test("a bit more complex regex", () => {
@@ -51,12 +78,202 @@ describe("filterDependencyTree", () => {
     );
 
     // Act
+    const result = filterDependencyTree(dependencyTreeObject, [
+      "org.obj.*:jar:\\d.*test"
+    ]);
+
+    // Assert
+    expect(result.length).toBe(2);
+  });
+
+  test("multiple one matching", () => {
+    // Arrange
+    const dependencyTreeObject = getDependencyTreeObjectFromFile(
+      path.join(__dirname, "..", "resources", "simple.log")
+    );
+
+    // Act
+    const result = filterDependencyTree(dependencyTreeObject, [
+      "junit:junit:jar",
+      "org.mockito:mockito-core",
+      "org.slf4j:slf4j-api"
+    ]);
+
+    // Assert
+    expect(result.length).toBe(1);
+    expect(result[0].moduleId).toBe("kie-soup-commons");
+  });
+
+  test("multiple both matching", () => {
+    // Arrange
+    const dependencyTreeObject = getDependencyTreeObjectFromFile(
+      path.join(__dirname, "..", "resources", "simple.log")
+    );
+
+    // Act
+    const result = filterDependencyTree(dependencyTreeObject, [
+      "junit:junit:jar",
+      "org.mockito:mockito-core"
+    ]);
+
+    // Assert
+    expect(result.length).toBe(2);
+  });
+
+  test("multiple none matching", () => {
+    // Arrange
+    const dependencyTreeObject = getDependencyTreeObjectFromFile(
+      path.join(__dirname, "..", "resources", "simple.log")
+    );
+
+    // Act
+    const result = filterDependencyTree(dependencyTreeObject, [
+      "junit:junit:jar",
+      "org.mockito:mockito-core",
+      "x:X"
+    ]);
+
+    // Assert
+    expect(result.length).toBe(0);
+  });
+
+  test("filter empty", () => {
+    // Arrange
+    const dependencyTreeObject = getDependencyTreeObjectFromFile(
+      path.join(__dirname, "..", "resources", "simple.log")
+    );
+
+    // Act
+    const result = filterDependencyTree(dependencyTreeObject, []);
+
+    // Assert
+    expect(result.length).toBe(2);
+  });
+
+  test("filter undefined", () => {
+    // Arrange
+    const dependencyTreeObject = getDependencyTreeObjectFromFile(
+      path.join(__dirname, "..", "resources", "simple.log")
+    );
+
+    // Act
+    const result = filterDependencyTree(dependencyTreeObject, undefined);
+
+    // Assert
+    expect(result.length).toBe(2);
+  });
+
+  test("filter && exclude empty", () => {
+    // Arrange
+    const dependencyTreeObject = getDependencyTreeObjectFromFile(
+      path.join(__dirname, "..", "resources", "simple.log")
+    );
+
+    // Act
+    const result = filterDependencyTree(dependencyTreeObject, [], []);
+
+    // Assert
+    expect(result.length).toBe(2);
+  });
+
+  test("filter && exclude undefined", () => {
+    // Arrange
+    const dependencyTreeObject = getDependencyTreeObjectFromFile(
+      path.join(__dirname, "..", "resources", "simple.log")
+    );
+
+    // Act
     const result = filterDependencyTree(
       dependencyTreeObject,
-      "org.obj.*:jar:\\d.*test"
+      undefined,
+      undefined
     );
 
     // Assert
     expect(result.length).toBe(2);
+  });
+});
+
+describe("filterDependencyTree with exclude", () => {
+  test("simple regex 0 matching and 1 non-existing excluded", () => {
+    // Arrange
+    const dependencyTreeObject = getDependencyTreeObjectFromFile(
+      path.join(__dirname, "..", "resources", "simple.log")
+    );
+
+    // Act
+    const result = filterDependencyTree(dependencyTreeObject, ["x:x"], ["y:y"]);
+
+    // Assert
+    expect(result.length).toBe(0);
+  });
+
+  test("simple regex 1 matching and 1 excluded", () => {
+    // Arrange
+    const dependencyTreeObject = getDependencyTreeObjectFromFile(
+      path.join(__dirname, "..", "resources", "simple.log")
+    );
+
+    // Act
+    const result = filterDependencyTree(
+      dependencyTreeObject,
+      ["jakarta.transaction-api:jar:\\d"],
+      ["jakarta.transaction-api:jar:\\d"]
+    );
+
+    // Assert
+    expect(result.length).toBe(0);
+  });
+
+  test("simple regex 1 matching and 1 excluded not existing excluded", () => {
+    // Arrange
+    const dependencyTreeObject = getDependencyTreeObjectFromFile(
+      path.join(__dirname, "..", "resources", "simple.log")
+    );
+
+    // Act
+    const result = filterDependencyTree(
+      dependencyTreeObject,
+      ["jakarta.transaction-api:jar:\\d"],
+      ["x:X"]
+    );
+
+    // Assert
+    expect(result.length).toBe(1);
+  });
+
+  test("multiple both matching 1 excluded", () => {
+    // Arrange
+    const dependencyTreeObject = getDependencyTreeObjectFromFile(
+      path.join(__dirname, "..", "resources", "simple.log")
+    );
+
+    // Act
+    const result = filterDependencyTree(
+      dependencyTreeObject,
+      ["junit:junit:jar", "org.mockito:mockito-core"],
+      ["org.slf4j:slf4j-api"]
+    );
+
+    // Assert
+    expect(result.length).toBe(1);
+    expect(result[0].moduleId).toBe("kie-soup-parent");
+  });
+
+  test("multiple both matching both excluded", () => {
+    // Arrange
+    const dependencyTreeObject = getDependencyTreeObjectFromFile(
+      path.join(__dirname, "..", "resources", "simple.log")
+    );
+
+    // Act
+    const result = filterDependencyTree(
+      dependencyTreeObject,
+      ["junit:junit:jar", "org.mockito:mockito-core"],
+      ["org.hamcrest:hamcrest-core"]
+    );
+
+    // Assert
+    expect(result.length).toBe(0);
   });
 });
