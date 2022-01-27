544bfb539cf551f1a3efcf5db96fd32a41fb6798
Roberto Oliveira <rguimara@redhat.com>
2022-01-27 14:33:16 +0100

    add github-action.test.js

--- src/bin/github-action.js
+++ src/bin/github-action.js
@@ -1,16 +1,23 @@
 #!/usr/bin/env node
 const core = require("@actions/core");
 const { main } = require("./main");
-const args = {
-  logFilePath: core.getInput("log-file-path"),
-  outputFile: core.getInput("output-file"),
-  skipOutput: core.getInput("skip-output") ? true : false,
-  filter: core.getInput("filter"),
-  exclude: core.getInput("exclude"),
-  artifacts: core.getInput("artifacts")
-    ? core.getInput("artifacts").split(";")
-    : undefined,
-  printModuleList: core.getInput("print-module-list") ? true : false,
-  debug: core.getInput("logger-level") === "debug"
+
+const start = () => {
+  const args = {
+    logFilePath: core.getInput("log-file-path"),
+    outputFile: core.getInput("output-file"),
+    skipOutput: Boolean(core.getInput("skip-output")),
+    filter: core.getInput("filter"),
+    exclude: core.getInput("exclude"),
+    artifacts: core.getInput("artifacts"),
+    printModuleList: Boolean(core.getInput("print-module-list")),
+    debug: core.getInput("logger-level") === "debug"
+  };
+  main(args);
 };
-main(args);
+
+if (require.main === module) {
+  start();
+}
+
+module.exports = { start };
